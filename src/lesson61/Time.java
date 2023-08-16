79a6beded4e90f52f131d3554dd9bb45e023662d
Ustinya Petrova <ust.petrova@gmail.com>
2023-08-16 21:23:42 +0200

    feat: add Time draft

---
+++ src/lesson61/Time.java
@@ -0,0 +1,26 @@
+package lesson61;
+
+import java.util.Scanner;
+
+public class Time {
+
+  // по введённому размеру выводить оценку эффективности:
+  // по времени и по памяти (без доп. коэффициентов)
+  // O(n) = n, O(n^2) = n^2
+
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    System.out.print("Введите размер данных: ");
+    long n = scanner.nextLong();
+    printTimeEfficiency(n);
+    printSpaceEfficiency(n);
+  }
+
+  private static void printTimeEfficiency(long n) {
+
+  }
+
+  private static void printSpaceEfficiency(long n) {
+
+  }
+}
