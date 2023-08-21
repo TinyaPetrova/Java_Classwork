9763be6d63468e77ac84125c5b8d8b1e9b16ad2d
Ustinya Petrova <ust.petrova@gmail.com>
2023-08-21 20:11:23 +0200

    feat: add Order.calculatePrice()

--- src/lesson63/hair_color/Order.java
+++ src/lesson63/hair_color/Order.java
@@ -5,6 +5,11 @@ import java.util.Scanner;
 
 public class Order {
 
+  private static final double PRICE_PER_CM = 1.5; // евро за см длины
+
+  private static final double MULTIPLIER_PER_СOLOR = 0.2; // 20% за каждый цвет
+
+
   private String name; // имя заказчика
 
   private LocalTime startTime; // время начала
@@ -22,6 +27,7 @@ public class Order {
     this.startTime = startTime;
     this.colors = colors;
     this.length = length;
+    calculatePrice();
   }
 
   public String getName() {
@@ -64,6 +70,20 @@ public class Order {
     this.price = price;
   }
 
+  /**
+   * Обновление поля <code>price</code> с использованием значений полей <code>length</code> и
+   * <code>colors</code>
+   */
+  private void calculatePrice() {
+    price = PRICE_PER_CM * length + MULTIPLIER_PER_СOLOR * colors;
+  }
+
+  /**
+   * Интерактивное чтение данных о заказе
+   *
+   * @param scanner источник данных
+   * @return прочитанный заказ
+   */
   public static Order interactiveRead(Scanner scanner) {
     System.out.print("Введите имя заказчика: ");
     String name = scanner.nextLine();
