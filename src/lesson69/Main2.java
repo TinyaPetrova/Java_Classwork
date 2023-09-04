db9c579fca25f148a439a0a56555f83c78f795f9
Ustinya Petrova <ust.petrova@gmail.com>
2023-09-04 21:10:09 +0200

    feat: add join()

--- src/lesson69/Main2.java
+++ src/lesson69/Main2.java
@@ -9,15 +9,16 @@ public class Main2 extends Thread {
     }
   }
 
-  public static void main(String[] args) {
+  public static void main(String[] args) throws InterruptedException {
     Thread thread = new Main2();
     Thread myThread = new MyThread(50);
     thread.start();
-    myThread.start();
     for (int i = 0; i < 50; i++) {
       System.out.println(i + ". I'm the main thread");
       System.out.println(i + ". I'm the main thread");
       System.out.println(i + ". I'm the main thread");
     }
+    thread.join(); // подождать завершение потока
+    myThread.start();
   }
 }
